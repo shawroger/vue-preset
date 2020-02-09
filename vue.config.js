7fd19f41152136e413f1d0997ff74f3315a70f8c
Roger <47489692+shawroger@users.noreply.github.com>
2020-02-09 14:30:29 +0800

    Create vue.config.js

---
+++ vue.config.js
@@ -0,0 +1,25 @@
+const path = require("path");
+
+module.exports = {
+	pluginOptions: {
+		"style-resources-loader": {
+			preProcessor: "less",
+			patterns: [path.resolve(__dirname, "src/assets/global.less")]
+		}
+	},
+	chainWebpack: config => {
+		config.when(process.env.NODE_ENV === "development", config => {
+			config.plugin("html").tap(args => {
+				args[0].isDev = true;
+				return args;
+			});
+		});
+
+		config.when(process.env.NODE_ENV === "production", config => {
+			config.plugin("html").tap(args => {
+				args[0].isDev = fasle;
+				return args;
+			});
+		});
+	}
+};
